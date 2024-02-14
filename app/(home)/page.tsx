b8be7c2144fd9e55d47ca19d0ac7e36d453de638
palomacorrea <paloma.correa@dnkinfotelecom.com.br>
2024-02-14 16:53:55 -0300

    feat: add welcome msg

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
 import Header from "../_components/header";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
+
 
 export default function Home() {
   return (//utilizando o componente Header criado
     <div>
       <Header/>
+      <div className="px-5 pt-5">
+        <h2 className="text -xl font-bold">Olá, Fulano!</h2>
+        <p className="capitalize">{format(new Date(), "EEEE','dd 'de' MMMM'.'", {
+          locale: ptBR
+          })}</p>
+      </div>
     </div>
 
   );
