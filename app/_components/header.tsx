c8661e6455c4c71533c46f98f5766ed841feca64
palomacorrea <paloma.correa@dnkinfotelecom.com.br>
2024-02-14 15:13:58 -0300

    feat: including header page

---
+++ app/_components/header.tsx
@@ -0,0 +1,18 @@
+import { Card, CardContent } from "./ui/card";
+import Image from "next/image";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+const Header = () => {
+    return (
+        <Card>
+            <CardContent>
+            <Image src="/logo.png" alt="Logo Barber" height={22} width={120}/>
+            <Button variant="outline"size="icon">
+                <MenuIcon/>
+            </Button>
+            </CardContent>
+        </Card>
+    );
+}
+
+export default Header;
